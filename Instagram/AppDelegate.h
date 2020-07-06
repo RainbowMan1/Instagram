19574e46744ac72d06d970ee30131750938aed95
RainbowMan1 <nikeshsubedi2@gmail.com>
2020-07-06 18:18:45 -0400

    Initial Commit

---
+++ Instagram/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Nikesh Subedi on 7/6/20.
+//  Copyright © 2020 Nikesh Subedi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Instagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Instagram
+//
+//  Created by Nikesh Subedi on 7/6/20.
+//  Copyright © 2020 Nikesh Subedi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
