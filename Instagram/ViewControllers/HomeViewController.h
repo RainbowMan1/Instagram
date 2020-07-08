0269d3393edd70a6078d22b4ad5a35d67a335ee4
RainbowMan1 <nikeshsubedi2@gmail.com>
2020-07-07 22:06:30 -0400

    Added Login and Sign Up feature

---
+++ Instagram/ViewControllers/HomeViewController.h
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.h
+//  Instagram
+//
+//  Created by Nikesh Subedi on 7/7/20.
+//  Copyright © 2020 Nikesh Subedi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HomeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Instagram/ViewControllers/HomeViewController.m
@@ -0,0 +1,42 @@
+//
+//  HomeViewController.m
+//  Instagram
+//
+//  Created by Nikesh Subedi on 7/7/20.
+//  Copyright © 2020 Nikesh Subedi. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()<UITableViewDataSource, UITableViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    return [[UITableViewCell alloc]init];
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+
+
+@end
